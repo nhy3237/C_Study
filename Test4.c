d2d6cee339ae4c5f75f1078f428cd475db6b228d
nhy3237 <njs3237@naver.com>
2023-05-25 10:24:22 +0900

    이름 변경

--- Test4.c
+++ Test4.c
@@ -5,52 +5,15 @@
 
 int main()
 {
-    //int N, rank, x[50], y[50]; // 전체 사람 수, 덩치 등수, 키, 몸무게
+    int T;
+    char str[20][1000];
 
-    //scanf("%d", &N);
+    scanf("%d", &T);
 
-    //for (int i = 0; i < N; i++)
-    //{
-    //   scanf("%d %d", &x[i], &y[i]);
-    //}
-
-    //for (int i = 0; i < N; i++)
-    //{
-    //   rank = 1;
-    //   for (int j = 0; j < N; j++)
-    //   {
-    //      if (i == j) continue;
-    //      if (x[i] < x[j] && y[i] < y[j])
-    //      {
-    //         rank += 1;
-    //      }
-    //   }
-    //   printf("%d ", rank);
-    //}
-
-    char N[8]; // 방 번호
-    int num[10] = { 0 }; // 0~9까지의 정수 개수 구하는 배열
-    int length, cntSet = 0; // 방 번호 길이, 세트 개수
-
-    scanf("%s", &N);
-
-    length = strlen(N);
-
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < 2; i++)
     {
-        num[N[i] - '0']++; // 문자 상수 '0' ~ '9' = 아스키 코드 48 ~ 57
+        scanf("%s", str[i]);
     }
-     
-    num[9] = (num[9] += num[6] + 1) / 2;
-
-    for (int i = 0; i < 10; i++)
-    {
-        if (i == 6) continue;
-
-        if (cntSet < num[i]) cntSet = num[i];
-    }
-
-    printf("%d", cntSet);
 
     return 0;
 }
